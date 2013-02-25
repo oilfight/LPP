ae4ffc20f6cdb980d2bbb3201b89918ce92ed614
snamber <stefan.amberger@jku.at>
2013-02-25 16:42:16 +0100

    modified install script

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 if test "`id -u`" -ne 0
 	then 
-	echo "You need to run this script as root" 
+	echo "ERROR: You need to run this script as root" 
 	exit 
 fi
 
 echo "checking for old installations..."
 if [[ -L /usr/bin/lpp ]]; then
-	echo "you need to uninstall old lpp versions first."
+	echo "ERROR: you need to uninstall old lpp versions first."
 	echo "you can do by so executing"
 	echo "sudo rm /usr/bin/lpp /usr/bin/pizza"
 	exit 1
